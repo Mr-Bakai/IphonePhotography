1a579cf62792e1971f5db15d1365d670f9bdc135
Mr-Bakai <ismailov.time@gmail.com>
2023-06-26 19:49:06 +0600

    Initial Commit

---
+++ IphonePhotography/IphonePhotographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphonePhotographyApp.swift
+//  IphonePhotography
+//
+//  Created by Bakai on 26/6/23.
+//
+
+import SwiftUI
+
+@main
+struct IphonePhotographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
